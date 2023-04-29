af066e65c6aae5111a91208f52e16daedee9e91d
abhidharshss <abhidharsh1999@gmail.com>
2023-04-29 07:29:05 +0530

    djikstras1

---
+++ graph/Djikstra's.py
@@ -0,0 +1,16 @@
+class edge:
+    def __init__(self,weight,start_vertex,target_vertex):
+        self.weight=weight
+        self.start_vertex=start_vertex
+        self.target_vertex=target_vertex
+
+class Node:
+    def __init__(self,name):
+        self.name=name
+        self.visited=False
+        #pervious node that we come to the node
+        self.predecessor=None
+        self.neighbours=[]
+        self.min_distance=float('inf')
+    def __lt__(self,other_node):
+        return self.min_distance < other_node.min_distance
