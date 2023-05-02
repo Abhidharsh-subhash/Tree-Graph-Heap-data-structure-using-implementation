729443416d7fb0af439e1b233b554e0414c2f2e8
abhidharshss <abhidharsh1999@gmail.com>
2023-05-02 13:11:55 +0530

    binary tree deletion completed

--- Tree/binarytree/BTdeletion.py
+++ Tree/binarytree/BTdeletion.py
@@ -16,23 +16,84 @@ fanta=Graph('fanta')
 drinks.right=cold
 drinks.left=hot
 hot.left=tea
-hot.left=coffee
 hot.right=coffee
 cold.right=fanta
+cold.left=cola
 
-#get the deepest value to in the tree 
-def getdeepest(rootnode):
+
+def levelorder(rootnode):
     if not rootnode:
         return
     else:
-        customqueue=queue.Queue()
-        customqueue.enqueue(rootnode)
-        while not (customqueue.isEmpty()):
-            root=customqueue.dequeue()
+        custom=queue.Queue()
+        custom.enqueue(rootnode)
+        while not (custom.isEmpty()):
+            root=custom.dequeue()
+            print(root.value.data)
             if root.value.left is not None:
-                customqueue.enqueue(root.value.left)
+                custom.enqueue(root.value.left)
             if root.value.right is not None:
-                customqueue.enqueue(root.value.right)
-        return root.value.data
+                custom.enqueue(root.value.right)
+#get the deepest value to in the tree 
+def deepest(rootnode):
+    if not rootnode:
+        return
+    else:
+        custom=queue.Queue()
+        custom.enqueue(rootnode)
+        while not (custom.isEmpty()):
+            root=custom.dequeue()
+            if root.value.left is not None:
+                custom.enqueue(root.value.left)
+            if root.value.right is not None:
+                custom.enqueue(root.value.right)
+        deepestnode=root.value
+        return deepestnode
+    
+def deletedeepest(rootnode,node):
+    if not rootnode:
+        return
+    else:
+        custom=queue.Queue()
+        custom.enqueue(rootnode)
+        while not (custom.isEmpty()):
+            root=custom.dequeue()
+            if root.value is node:
+                root.value=None
+                return
+            if root.value.left:
+                if root.value.left is node:
+                    root.value.left=None
+                    return
+                else:
+                    custom.enqueue(root.value.left)
+            if root.value.right:
+                if root.value.right is node:
+                    root.value.right=None
+                    return
+                else:
+                    custom.enqueue(root.value.right)
 
-print(getdeepest(drinks))
+def deletenode(rootnode,node):
+    if not rootnode:
+        return
+    else:
+        custom=queue.Queue()
+        custom.enqueue(rootnode)
+        while not (custom.isEmpty()):
+            root=custom.dequeue()
+            if root.value.data == node:
+                dnode=deepest(rootnode)
+                root.value.data=dnode.data
+                deletedeepest(rootnode,dnode)
+                return 'deleted successfully'
+            if root.value.left is not None:
+                custom.enqueue(root.value.left)
+            if root.value.right is not None:
+                custom.enqueue(root.value.right)
+        return 'deletion failed'
+    
+levelorder(drinks)
+print('after deletion')
+print(deletenode(drinks,'coffee'))
+levelorder(drinks)
