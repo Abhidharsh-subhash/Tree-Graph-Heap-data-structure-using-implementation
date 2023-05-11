a5890dc1669378798076dbd5072e961b338203e5
abhidharshss <abhidharsh1999@gmail.com>
2023-05-11 20:24:18 +0530

    djikstras

--- graph/Djikstra's.py
+++ graph/Djikstra's.py
@@ -12,5 +12,10 @@ class Node:
         self.predecessor=None
         self.neighbours=[]
         self.min_distance=float('inf')
+    #normally we can't compare the nodes so inorder to compare the nodes we use the method __lt__
     def __lt__(self,other_node):
         return self.min_distance < other_node.min_distance
+    #adding the edge between the nodes
+    def addedge(self,weight,destination_vertex):
+        edge=edge(weight,self,destination_vertex)
+        self.neighbours.append(edge)
