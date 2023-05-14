65765d5c90a4181ec1fe67f7ef258f5bf50d1216
abhidharshss <abhidharsh1999@gmail.com>
2023-05-14 21:12:36 +0530

    reference in heapsort

--- Tree/binarytree/binaryheap/heapsort.py
+++ Tree/binarytree/binaryheap/heapsort.py
@@ -12,14 +12,16 @@ def heapify(list,n,i):#n=>no.of elements in the list i=>index in the list
 
 def heapsort(array):                                #TIME COMPLEXITY:O(NlogN)
     n=len(array)
+    #this is for building the min heap
     for i in range(int(n/2),-1,-1):
         heapify(array,n,i)
+    #this is for deleting the element from min heap
     for i in range(n-1,0,-1):
         array[i],array[0]=array[0],array[i]
         heapify(array,i,0)
     #array.reverse()
 
-array=[4,7,2,5,7,8,3,1]
+array=[30,17,20,1,5,10]
 heapsort(array)
 print(array)
  
